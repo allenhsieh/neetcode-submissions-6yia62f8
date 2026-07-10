521e01f2e54b80b2e7fdf7c5835b266e33b17b95
Allen Hsieh <10136383+allenhsieh@users.noreply.github.com>
2026-07-10 16:22:38 -0700

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let map = new Map()
+        for (let i = 0; i < nums.length; i++) {
+            let difference = target - nums[i]
+            if (map.has(difference)) {
+                let coord = map.get(difference)
+                return [coord, i]
+            } else {
+                map.set(nums[i], i)
+            }
+        }
+    }
+}
